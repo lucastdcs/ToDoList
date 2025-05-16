0c98c2d9e407492a14adab32669d0bd30c196fe0
Lucas <lucas.txsan@gmail.com>
2025-05-16 19:42:52 -0300

    fixed js logic

--- app.js
+++ app.js
@@ -4,12 +4,27 @@ const taskList = document.getElementById('task-list')
 
 addTaskBtn.addEventListener('click', addTask);
 
-function addTask(){
+function addTask() {
     const taskText = addTaskInput.value.trim();
 
+    if (taskText === '') {
+        alert('You didnt write your task')
+        return
+    }
+
     const li = document.createElement('li');
-    li.textContent = taskText;
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    li.appendChild(checkbox)
+    li.appendChild(document.createTextNode(`${taskText}`))
 
     taskList.appendChild(li)
     addTaskInput.value = '';
 }
+
+addTaskInput.addEventListener('keypress', function(e){
+    if(e.key == 'Enter'){
+        addTask();
+    }
+})
